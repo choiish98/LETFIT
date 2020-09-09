5e89d132cde0f0e7cde9a8f30d7b6a6e5b13731d
choiish98 <dnjsqls9119@gmail.com>
2020-09-09 23:14:47 +0900

    firebase 연동

--- app/src/main/java/com/example/letfit/MainActivity.java
+++ app/src/main/java/com/example/letfit/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.letfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        SignInButton button = (SignInButton) findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("click test");
+            }
+        });
     }
 }
