795e1e9f77ea92f0adce06b1d0e9f27ff5f4dbac
김소정 <sojeong7524@gmail.com>
2020-11-12 13:52:17 +0900

    육각형 가로줄 세로줄 없앰 밸류값 가림 색상 바꿈 알파값 추가 yaxis 속성 추가

--- app/src/main/java/com/example/letfit/MainActivity.java
+++ app/src/main/java/com/example/letfit/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.letfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+
+import java.util.ArrayList;
+
+import static android.graphics.Color.rgb;
 // 빨리 다운 로드 해라 이것들아;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +30,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RadarChart radarChart = findViewById(R.id.radarChart);
+        radarChart.getDescription().setEnabled(false);
+
+        radarChart.setWebColor(Color.WHITE);        // 세로줄 없애기
+        radarChart.setWebLineWidth(1f);
+        //radarChart.setWebColorInner(Color.WHITE);   // 가로줄 없애기
+        radarChart.setWebAlpha(100);
+        radarChart.getLegend().setTextColor(Color.WHITE);
+        radarChart.getLegend().setTextColor(Color.WHITE);
+
+        ArrayList<RadarEntry> visitorsForFirstWebsite = new ArrayList<>();
+        visitorsForFirstWebsite.add(new RadarEntry(350));
+        visitorsForFirstWebsite.add(new RadarEntry(250));
+        visitorsForFirstWebsite.add(new RadarEntry(200));
+        visitorsForFirstWebsite.add(new RadarEntry(400));
+        visitorsForFirstWebsite.add(new RadarEntry(250));
+
+        RadarDataSet radarDataSetForFirstWebsite = new RadarDataSet(visitorsForFirstWebsite, "website 1");
+        radarDataSetForFirstWebsite.setColor(rgb(150, 31, 47));
+        radarDataSetForFirstWebsite.setValueTextColor(rgb(150, 31, 47));
+        radarDataSetForFirstWebsite.setFillColor(rgb(150, 31, 47));
+        radarDataSetForFirstWebsite.setDrawFilled(true);
+        radarDataSetForFirstWebsite.setFillAlpha(3000);
+        radarDataSetForFirstWebsite.setDrawHighlightIndicators(false);
+        radarDataSetForFirstWebsite.setDrawHighlightCircleEnabled(true);
+        radarDataSetForFirstWebsite.setDrawValues(false);
+
+
+        RadarData radarData = new RadarData();
+        radarData.addDataSet(radarDataSetForFirstWebsite);
+
+        String[] labels = {"등", "가슴", "하체", "복부", "어깨"};
+
+        XAxis xAxis = radarChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        xAxis.setTextSize(12f);
+
+        YAxis yAxis = radarChart.getYAxis();
+        yAxis.setLabelCount(100, false);
+        yAxis.setDrawLabels(false);
+        yAxis.setAxisMaximum(500);
+        yAxis.setAxisMinimum(0);
+
+        radarChart.getDescription().setText("Radar Chart Example");
+        radarChart.setData(radarData);
+        radarChart.invalidate();
     }
 }
