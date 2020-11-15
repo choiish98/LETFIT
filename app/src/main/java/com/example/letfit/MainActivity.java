57adbef4a6cc2468c23174ea076940ae5b071e01
choiish98 <dnjsqls9119@gmail.com>
2020-11-15 18:51:28 +0900

    육각형 병합

--- app/src/main/java/com/example/letfit/MainActivity.java
+++ app/src/main/java/com/example/letfit/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 import com.github.mikephil.charting.charts.RadarChart;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.RadarData;
 import com.github.mikephil.charting.data.RadarDataSet;
 import com.github.mikephil.charting.data.RadarEntry;
@@ -19,6 +20,8 @@ import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
+
+import static android.graphics.Color.rgb;
 // 빨리 다운 로드 해라 이것들아;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,33 +37,51 @@ public class MainActivity extends AppCompatActivity {
 
         // 육각형 기능
         RadarChart radarChart = findViewById(R.id.radarChart);
+        radarChart.getDescription().setEnabled(false);
+
+        radarChart.setWebColor(Color.WHITE);        // 세로줄 없애기
+        radarChart.setWebLineWidth(1f);
+        radarChart.setWebColorInner(Color.WHITE);   // 가로줄 없애기
+        radarChart.setWebAlpha(100);
+        radarChart.getLegend().setTextColor(Color.WHITE);
+        radarChart.getLegend().setTextColor(Color.WHITE);
 
         ArrayList<RadarEntry> visitorsForFirstWebsite = new ArrayList<>();
-        visitorsForFirstWebsite.add(new RadarEntry(420));
-        visitorsForFirstWebsite.add(new RadarEntry(654));
-        visitorsForFirstWebsite.add(new RadarEntry(540));
-        visitorsForFirstWebsite.add(new RadarEntry(480));
-        visitorsForFirstWebsite.add(new RadarEntry(040));
-        visitorsForFirstWebsite.add(new RadarEntry(620));
+        visitorsForFirstWebsite.add(new RadarEntry(350));
+        visitorsForFirstWebsite.add(new RadarEntry(250));
+        visitorsForFirstWebsite.add(new RadarEntry(200));
+        visitorsForFirstWebsite.add(new RadarEntry(400));
+        visitorsForFirstWebsite.add(new RadarEntry(250));
 
         RadarDataSet radarDataSetForFirstWebsite = new RadarDataSet(visitorsForFirstWebsite, "website 1");
-        radarDataSetForFirstWebsite.setColor(Color.RED);
-        radarDataSetForFirstWebsite.setLineWidth(2f);
-        radarDataSetForFirstWebsite.setValueTextColor(Color.RED);
-        radarDataSetForFirstWebsite.setValueTextSize(14f);
+        radarDataSetForFirstWebsite.setColor(rgb(150, 31, 47));
+        radarDataSetForFirstWebsite.setValueTextColor(rgb(150, 31, 47));
+        radarDataSetForFirstWebsite.setFillColor(rgb(150, 31, 47));
         radarDataSetForFirstWebsite.setDrawFilled(true);
-        radarDataSetForFirstWebsite.setFillColor(Color.RED);
+        radarDataSetForFirstWebsite.setFillAlpha(3000);
+        radarDataSetForFirstWebsite.setDrawHighlightIndicators(false);
+        radarDataSetForFirstWebsite.setDrawHighlightCircleEnabled(true);
+        radarDataSetForFirstWebsite.setDrawValues(false);
+
 
         RadarData radarData = new RadarData();
         radarData.addDataSet(radarDataSetForFirstWebsite);
 
-        String[] labels = {"등", "어깨", "가슴", "팔", "복근", "하체"};
+        String[] labels = {"등", "가슴", "하체", "복부", "어깨"};
 
         XAxis xAxis = radarChart.getXAxis();
         xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        xAxis.setTextSize(12f);
+
+        YAxis yAxis = radarChart.getYAxis();
+        yAxis.setLabelCount(100, false);
+        yAxis.setDrawLabels(false);
+        yAxis.setAxisMaximum(500);
+        yAxis.setAxisMinimum(0);
 
         radarChart.getDescription().setText("Radar Chart Example");
         radarChart.setData(radarData);
+        radarChart.invalidate();
         // 육각형
 
         // 버튼 정의
